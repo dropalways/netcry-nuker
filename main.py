bf0362abac230baa061042260267815800a1c568
always <rifatxd12@gmail.com>
2023-07-12 08:58:28 +0100

    stuff looks good now
    fr

--- main.py
+++ main.py
@@ -7,19 +7,24 @@ import os
 import time
 import subprocess
 
+"""@a1lw and @loding_x coded this
+if you are going to skid the code keep this please and thank you mr skid man"""
+
 init()
 
 options = """
-+---------------------------------+--------------------------------+
-|                                 |  [8]                           |
-|  [1] Mass ban                   |  [9]                           |
-|  [2] Mass create channels       |  [10]                          |
-|  [3] Delete all channels        |  [11]                          |
-|  [4] Webhook spammer            |  [12]                          |
-|  [5]                            |  [13]                          |
-|  [6]                            |  [14]                          |
-|  [7]                            |  [15]                          |
-+---------------------------------+--------------------------------+
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃   Discord: @a1lw and @loding_x  ┃  github.com/dropalways/netcry  ┃
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+┃                                 ┃  [8]                           ┃
+┃  [1] Mass ban                   ┃  [9]                           ┃
+┃  [2] Mass create channels       ┃  [10]                          ┃
+┃  [3] Delete all channels        ┃  [11]                          ┃
+┃  [4] Webhook spammer            ┃  [12]                          ┃
+┃  [5]                            ┃  [13]                          ┃
+┃  [6]                            ┃  [14]                          ┃
+┃  [7]                            ┃  [15]                          ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 """
 
 def banner():
@@ -31,16 +36,16 @@ def banner():
 ░ ▒░   ▒ ▒ ░░ ▒░   ▒ ░░   ░ ░▒ ▒  ░ ▒▓ ░▒▓░   ██▒▒▒ 
 ░ ░░   ░ ▒░ ░ ░      ░      ░  ▒    ░▒ ░ ▒░ ▓██ ░▒░ 
    ░   ░ ░    ░    ░ ░    ░          ░   ░  ▒ ▒ ░░  
-         ░    ░           ░ ░        ░      ░ ░     \n"""
+         ░    ░           ░ ░        ░      ░ ░     """
     print(banner_text)
 
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
     os.system("title ball")
     banner()
-    print(Colorate.Vertical(Colors.blue_to_purple, options, 1))
+    print(Colorate.Vertical(Colors.purple_to_red, options, 1))
     while True:
-        user_input = Write.Input("\n\n--trollinc""@netcry->>> ", Colors.blue_to_red, interval=0.05)
+        user_input = Write.Input("\n\n━━trollinc""@netcry━>>> ", Colors.blue_to_red, interval=0.05)
         user_input = user_input.lower()
     
         if user_input == "1":
