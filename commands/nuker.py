d1e3f4d7d5e57ff601f6cb797ab6d0660f70bb0d
chris2rich <66845682+w3althy@users.noreply.github.com>
2023-07-18 13:31:58 -0400

    random changes

--- commands/nuker.py
+++ commands/nuker.py
@@ -2,10 +2,10 @@ import asyncio
 import aiohttp
 import threading
 
-#this is nearly as fast as nebula nuker bot lol
-#why are you reading the source code?
-#either you think it is a rat or you are trying to skid it hm
-#property of @a1lw on discord or https://github.com/dropalways
+# this is nearly as fast as nebula nuker bot lol
+# why are you reading the source code?
+# either you think it is a rat or you are trying to skid it hm
+# property of @a1lw on discord or https://github.com/dropalways
 
 print("This will only work with bot tokens")
 with open("token.txt", "r") as file:
