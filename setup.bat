78000a3ddda6d65ef46cef5758dc0e11cf6c1773
chris2rich <66845682+w3althy@users.noreply.github.com>
2023-07-30 12:31:10 -0400

    重写安装批处理脚本
    rewrite the setup batch script (setup.bat)

--- setup.bat
+++ setup.bat
@@ -1,7 +1,30 @@
 @echo off
-
 title Installing requirements...
 
+REM check if 'pip' is available in the system - w3althy
+where /q pip
+if %errorlevel% neq 0 (
+    echo Error: 'pip' command not found. Make sure Python is installed and is added to the system PATH.
+    pause
+    exit /b 1
+)
+
+REM install python packages from requirements.txt - w3althy
 pip install -r requirements.txt
+if %errorlevel% neq 0 (
+    echo Error: Failed to install Python packages. Check if the requirements.txt file is valid.
+    pause
+    exit /b 1
+)
+
+REM run the main.py script - w3althy
 py main.py
+if %errorlevel% neq 0 (
+    echo Error: Failed to run main.py. Check if the file exists.
+    pause
+    exit /b 1
+)
+
+echo Installation and execution completed successfully.
 pause
+exit /b 0
