9f2300ed2b2a7fa01e8cd8eeefd2c84dfe46f222
always <120043514+dropalways@users.noreply.github.com>
2023-11-14 10:53:00 +0000

    probably added a whitelist to ban all
    but i dont know if it works due to me not having a huge server

--- commands/massban.py
+++ commands/massban.py
@@ -1,6 +1,5 @@
 import requests
 import time
-import json
 import concurrent.futures
 
 
@@ -10,10 +9,10 @@ def ball():
     headers = {'Authorization': f'Bot {token}'}
     if token == "":
         print("Empty token")
-        print("Ignore the error below i dont know how to fix it if you know how to fix it create a pull request")
+        print("Ignore the error below; I don't know how to fix it. If you know how to fix it, create a pull request")
     elif token == "single token here":
-        print("You havent edited the file 'token.txt'.")
-        print("Ignore the error below i dont know how to fix it if you know how to fix it create a pull request")
+        print("You haven't edited the file 'token.txt'.")
+        print("Ignore the error below; I don't know how to fix it. If you know how to fix it, create a pull request")
     else:
         print("This will only work with bot tokens")
         massban(token, headers)
@@ -21,18 +20,39 @@ def ball():
 
 def massban(token, headers):
     guild_id = input("Enter the target guild id: ")
-    hm = requests.get(f'https://discord.com/api/v9/guilds/{guild_id}/members?limit=1000', headers=headers)
+    whitelist = input(
+        "Do you want to whitelist anyone from getting banned? (y/n): ")
+    whitelistids = []
+
+    if whitelist.lower() == "y":
+        while True:
+            user_id = input(
+                "Enter a user id you want to whitelist (or enter nothing to stop): ")
+            if not user_id:
+                break
+            whitelistids.append(user_id)
+    else:
+        pass
+
+    hm = requests.get(
+        f'https://discord.com/api/v9/guilds/{guild_id}/members?limit=1000', headers=headers)
     if hm.status_code == 200:
         members = hm.json()
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            ban_tasks = [executor.submit(ban_member, guild_id, member['user']['id'], token, headers) for member in members]
+            ban_tasks = [executor.submit(
+                ban_member, guild_id, member['user']['id'], token, headers, whitelistids) for member in members]
             concurrent.futures.wait(ban_tasks)
     else:
         print(f"Error code: {hm.status_code}")
 
 
-def ban_member(guild_id, user_id, token, headers):
-    response = requests.put(f'https://discord.com/api/v9/guilds/{guild_id}/bans/{user_id}', headers=headers)
+def ban_member(guild_id, user_id, token, headers, whitelistids):
+    if user_id in whitelistids:
+        print(f"User {user_id} is whitelisted and won't be banned.")
+        return
+
+    response = requests.put(
+        f'https://discord.com/api/v9/guilds/{guild_id}/bans/{user_id}', headers=headers)
     if response.status_code == 204:
         print(f"Banned {user_id}")
     elif response.status_code == 429:
